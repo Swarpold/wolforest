b714bb60185373f4a1dd6a98f373fd264b8f7d9a
Guillaume Messier <gui-ll-au-me@hotmail.fr>
2021-03-01 17:29:21 +0100

    ok

--- Wolf'Orest/Wolf'Orest/Assets/Scripts/mooveplayer.cs
+++ Wolf'Orest/Wolf'Orest/Assets/Scripts/mooveplayer.cs
@@ -1,15 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class mooveplayer : MonoBehaviour
 {
-    public float moveSpeed;
-
-    public Rigidbody2D rb;
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
