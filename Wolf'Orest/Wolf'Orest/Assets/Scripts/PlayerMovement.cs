2c984a286fdc3023574d012b4cf0763c87906e20
francois.halbout <francois;halbout@epita.fr>
2021-03-02 16:20:58 +0100

    ok

--- Wolf'Orest/Wolf'Orest/Assets/Scripts/PlayerMovement.cs
+++ Wolf'Orest/Wolf'Orest/Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
-        float horizonMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        MovePlayer(horizonMovement);
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        MovePlayer(horizontalMovement);
     }
     void MovePlayer(float _horizontalMovement)
     {
