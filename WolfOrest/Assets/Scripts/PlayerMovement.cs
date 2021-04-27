f5ef42c6ff4893128ec4e0e13879c3aea806302e
TheJuniored <francois.halbout@epita.fr>
2021-04-27 18:22:23 +0200

    goo

---
+++ WolfOrest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    //public float jumpForce;
+
+    //public bool isJumping = false;
+
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+
+
+    void FixedUpdate()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        /*if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }*/
+
+        MovePlayer(horizontalMovement);
+        MovePlayer2(verticalMovement);
+        rb.MovePosition(rb.position + new Vector2(1f,0f) * Time.fixedDeltaTime);
+    }
+
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        /*if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }*/
+    }
+
+    void MovePlayer2(float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(rb.velocity.x, _verticalMovement);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    }
+}
