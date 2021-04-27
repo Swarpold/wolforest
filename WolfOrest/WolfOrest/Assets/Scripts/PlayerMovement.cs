476b5fae17c58b0da174188fcf2a0d0e78980221
Yrgon <gui-ll-au-me@hotmail.fr>
2021-04-27 19:02:35 +0200

    Revert "multi"
    This reverts commit ae51f8b644f588d3525294109bac9d47c8f08a14.

--- WolfOrest/WolfOrest/Assets/Scripts/PlayerMovement.cs
+++ WolfOrest/WolfOrest/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using Photon.Pun;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,14 +9,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-
-
+   
+    
 
 
 
@@ -33,7 +26,6 @@ public class PlayerMovement : MonoBehaviour
 
         MovePlayer(horizontalMovement);
         MovePlayer2(verticalMovement);
-        rb.MovePosition(rb.position + new Vector2(1f,0f) * Time.fixedDeltaTime);
     }
 
 
